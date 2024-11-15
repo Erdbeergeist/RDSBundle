e0d767bdd0efc9143ac1fb2c6e68fa06a86a6c64
Erdbeergeist <luxmitges@web.de>
2024-11-15 18:52:10 +0100

    incredibly bad rust code to write_data_object

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,10 +1,64 @@
 use extendr_api::prelude::*;
+use std::fs::File;
+use std::io::{Read, Seek, SeekFrom, Write};
+use zstd::stream::{copy_decode, copy_encode};
 
-/// Return string `"Hello world!"` to R.
-/// @export
+//Compress stream
+fn compress_data(input: &[u8]) -> std::io::Result<Vec<u8>> {
+    let mut comp_data: Vec<u8> = Vec::new();
+    copy_encode(input, &mut comp_data, 0)?;
+
+    Ok(comp_data)
+}
+
+//Decompress stream
+fn decompress_data(input: &[u8]) -> std::io::Result<Vec<u8>> {
+    let mut uncomp_data: Vec<u8> = Vec::new();
+    copy_decode(input, &mut uncomp_data)?;
+
+    Ok(uncomp_data)
+}
+
+/// Write the compressed data
 #[extendr]
-fn hello_world() -> &'static str {
-    "Hello world!"
+fn write_data_object(file_path: String, ser_obj: Robj, offset: usize) -> extendr_api::Result<Robj> {
+    let serialized_object: Vec<u8> = ser_obj.try_into().unwrap();
+
+    let compressed_object = compress_data(&serialized_object);
+
+    let compressed_object = match compressed_object {
+        Ok(f) => f,
+        Err(e) => {
+            return Err(extendr_api::Error::from(format!(
+                "Error compressing: {}",
+                e
+            )))
+        }
+    };
+
+    let mut file = File::options()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&file_path);
+
+    let mut file = match file {
+        Ok(f) => f,
+        Err(e) => {
+            return Err(extendr_api::Error::from(format!(
+                "Error opening file: {}",
+                e
+            )))
+        }
+    };
+
+    file.seek(SeekFrom::Start(offset as u64))
+        .map_err(|e| Robj::from(format!("Error seeking in file: {}", e)));
+
+    file.write_all(&compressed_object)
+        .map_err(|e| Robj::from(format!("Error writing in file: {}", e)));
+
+    Ok(Robj::from(compressed_object.len()))
 }
 
 // Macro to generate exports.
@@ -12,5 +66,5 @@ fn hello_world() -> &'static str {
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod rdsb;
-    fn hello_world;
+    fn write_data_object;
 }
